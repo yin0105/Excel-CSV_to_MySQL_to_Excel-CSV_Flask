e79c94f30fc5dab93eb0630c540c7211cf74c360
solutionLi <momo821123@outlook.com>
2020-10-16 23:43:48 +0900

    Complete

--- app.py
+++ app.py
@@ -168,6 +168,7 @@ def to_mysql(db_name, tbl_name):
                             val_arr.append(str(row[c]))
 
                         db.engine.execute(sql_2, tuple(val_arr))
+                in_file.close()
             else:
                 book = xlrd.open_workbook(file_path)
                 sheet = book.sheet_by_index(0)
@@ -201,7 +202,6 @@ def to_mysql(db_name, tbl_name):
 @app.route('/to_excel/<string:db_name>/<string:tbl_name>', methods=['GET', 'POST'])
 def to_excel(db_name, tbl_name):
     col_list = Columns.query.filter(Columns.table_schema==db_name, Columns.table_name==tbl_name).all()
-	
     output = io.BytesIO()
 	
     #create WorkBook object
@@ -234,236 +234,47 @@ def to_excel(db_name, tbl_name):
     output.seek(0)
 
     return Response(output, mimetype="application/ms-excel", headers={"Content-Disposition":"attachment;filename=" + tbl_name + ".xls"})
-    
-    
-    # $sql = "SELECT column_name FROM COLUMNS WHERE table_schema='" . $_GET["db"] . "' AND table_name='" . $_GET["tbl"] . "'";
-    # if($result = mysqli_query($link, $sql)) {
-    #     if(mysqli_num_rows($result) > 0) {
-    #         while($row = mysqli_fetch_array($result)){
-    #             $res.='<div class="list-group-item"><label><input type="checkbox"><span class="list-group-item-text">&nbsp;&nbsp;&nbsp;' . $row[0] . '</span></label></div>';
-    #         }
-    #         mysqli_free_result($result);
-    #     }
-    # }
-
-
+        
 
 @app.route('/to_csv/<string:db_name>/<string:tbl_name>', methods=['GET', 'POST'])
 def to_csv(db_name, tbl_name):
-    pass
+    col_list = Columns.query.filter(Columns.table_schema==db_name, Columns.table_name==tbl_name).all()
+    output = io.StringIO()	
+    writer = csv.writer(output)
+		
+		# line = ['Emp Id, Emp First Name, Emp Last Name, Emp Designation']
+		# writer.writerow(line)
+
+		# for row in result:
+		# 	line = [str(row['emp_id']) + ',' + row['emp_first_name'] + ',' + row['emp_last_name'] + ',' + row['emp_designation']]
+		# 	writer.writerow(line)
 
 
 
+	
+    #add a sheet
+    sql = "SELECT "
+    col_arr = []
+    for col in col_list:
+        # print(col.column_name)	
+        if col.column_name != "id":	
+            col_arr.append(col.column_name)
+            sql += "`" + col.column_name + "`, "
+    writer.writerow(col_arr)
+    sql = sql[:-2] + " FROM " + db_name + ".`" + tbl_name + "`"
+    result = db.engine.execute(sql)
 
+    for row in result:
+        col_arr = []
+        for cell in row:
+            col_arr.append(str(cell))
+        writer.writerow(col_arr)
 
+	#add headers
+    output.seek(0)
 
+    return Response(output, mimetype="text/csv", headers={"Content-Disposition":"attachment;filename=" + tbl_name + ".csv"})
 
-
-
-
-
-
-    # def category_init_func(row):
-    #     c = Category(row['name'])
-    #     c.id = row['id']
-    #     return c
-    # def post_init_func(row):
-    #     c = Category.query.filter_by(name=row['category']).first()
-    #     p = Post(row['title'], row['body'], c, row['pub_date'])
-    #     return p
-    # request.save_book_to_database(
-    #     field_name='f_name', session=db.session,
-    #     tables=[],
-    #     initializers=[])
-    # return redirect(url_for('admin'), code=302)
-
-
-
-# @app.route('/login', methods = ['POST', 'GET'])
-# def login():
-#     # print(request.form['name'] + "::::::" + request.form['password'])
-#     if request.method == 'POST':
-#         if os.environ.get('ADMIN_NAME') == request.form['name'] and os.environ.get('ADMIN_PASSWORD') == request.form['password']:
-#             session['username'] = request.form['name']
-#             return redirect(url_for('admin'))
-   
-#     return render_template('login.html', form=LoginForm())
-
-# @app.route('/logout', methods = ['POST', 'GET'])
-# def logout():
-#     session.pop("username", None)
-#     return redirect(url_for("login"))
-
-# @app.route('/add_user', methods=['GET', 'POST'])
-# def add_user():
-#     form = UserForm(request.form)
-    
-#     if 'type_' in request.form:# == "save":
-
-#         if not form.validate_on_submit():
-#             flash('Please enter all the fields', 'error')
-#         else:
-#             str = ''
-#             for i in range(len(request.form.getlist('td_search[]'))):
-#                 if request.form.getlist('td_search[]')[i]:
-#                     if str != '' :
-#                         str += ','
-#                     str += '{"s": "' + request.form.getlist('td_search[]')[i] +'", '
-#                     str += '"m": "' + request.form.getlist('td_miles[]')[i] +'", '
-#                     str += '"t": "' + request.form.getlist('td_time[]')[i] +'"}'
-#             str = '{"locationList":[' + str +']}'
-
-#             user_ = User(request.form['name'], request.form['user_id'], request.form['password'], request.form['email'], request.form['phone'], request.form['dates'], str)
-            
-#             db.session.add(user_)
-#             db.session.commit()
-#             flash('Record was successfully added')
-#             return redirect(url_for('admin'))   
-    
-#     form.locations = [{"search": "", "miles": "", "time": ""}]
-#     return render_template('user.html', form=form, )
-
-
-# @app.route('/edit_user', methods=['POST'])
-# def edit_user():  
-#     form = UserForm(request.form)
-    
-#     # if request.method == 'POST':
-#     if request.form['type_'] == "save":
-#         if not form.validate_on_submit():
-#             flash('Please enter all the fields', 'error')
-#         else:
-#             str = ''
-#             for i in range(len(request.form.getlist('td_search[]'))):
-#                 if request.form.getlist('td_search[]')[i]:
-#                     if str != '' :
-#                         str += ','
-#                     str += '{"s": "' + request.form.getlist('td_search[]')[i] +'", '
-#                     str += '"m": "' + request.form.getlist('td_miles[]')[i] +'", '
-#                     str += '"t": "' + request.form.getlist('td_time[]')[i] +'"}'
-#             str = '{"locationList":[' + str +']}'
-           
-            
-#             db.session.query(User).filter_by(id = request.form['id']).update({User.name: request.form['name'], User.user_id: request.form['user_id'], User.password: request.form['password'], User.email: request.form['email'], User.phone: request.form['phone'], User.dates: request.form['dates'], User.locations: str}, synchronize_session = False)
-#             db.session.commit()
-#             flash('Record was successfully updated')
-#             return redirect(url_for('admin'))   
-#     else:
-#         user_ = User.query.filter_by(id=request.form['user_id']).first()
-#     user_.locations = json.loads(user_.locations)
-#     return render_template('user.html', form=form, user=user_)
-
-
-# @app.route('/del_user/<int:user_id>', methods=['GET', 'POST'])
-# def del_user(user_id):
-#     db.session.query(User).filter_by(id=user_id).delete()
-#     db.session.commit()
-
-#     return ""
-
-
-# @app.route('/view_log', methods=['POST'])
-# def view_log():  
-#     log_list = []
-#     try:
-#         log_file = open('logs/' + request.form['user_id'] + '.log', 'r') 
-#         while True: 
-#             line = log_file.readline()                
-#             if not line: 
-#                 break
-#             if line.find("/") < 0:
-#                 log_list.append(line.strip())                       
-#     except:
-#         pass
-#     return render_template('log.html', log_list = log_list)
-
-
-# @app.route('/calendar', methods=['GET', 'POST'])
-# def calendar():
-#     return render_template('calendar.html')
-
-
-# @app.route('/ajax_get_user_status', methods=['GET', 'POST'])
-# def ajax_get_user_status():
-#     users = User.query.order_by(User.name)
-#     result = ""
-#     for user_ in users:
-#         if str(user_.id) in proxy_status:
-#             result += str(proxy_status[str(user_.id)]) + ","
-#         else:
-#             result += "0,"
-#     result = result[:-1]
-
-#     return result
-
-
-# @app.route('/start_proxy/<userId>', methods=['GET', 'POST'])
-# def start_proxy(userId):
-#     try:
-#         if proxy_status[userId] >= 1:
-#             return ""
-#     except:
-#         pass
-
-#     proxy_status[userId] = 1
-#     print("proxy_status[" + userId + "] = " + str(proxy_status[userId]))
-#     db.session.query(User).filter_by(id = userId).update({User.status: 1}, synchronize_session = False)
-#     db.session.commit()
-
-#     user_ = User.query.filter_by(id=userId).first()
-#     user_.locations = json.loads(user_.locations)
-
-#     t = MyThread(userId, user_.to_dict())
-#     t.start()
-
-#     return ""
-        
-
-# @app.route('/stop_proxy/<userId>', methods=['GET', 'POST'])
-# def stop_proxy(userId):
-#     proxy_status[userId] = 0
-#     db.session.query(User).filter_by(id = userId).update({User.status: 0}, synchronize_session = False)
-#     db.session.commit()
-#     return ""
-
-
-# def status_initialize():
-#     db.session.query(User).update({User.status: 0}, synchronize_session = False)
-#     db.session.commit()
-#     return
-
-
-# def get_proxies_list():
-#     if os.environ.get('FREE_PROXY') != "true":
-#         try:
-#             proxies_file = open('proxies.txt', 'r') 
-#             while True: 
-#                 line = proxies_file.readline()                
-#                 if not line: 
-#                     break
-#                 proxies_list.append(line.strip())
-#             print("\n:::::::::::  Paid Proxy  ::::::::\n")
-#             return
-#         except:
-#             pass
-#     print("\n:::::::::::  Free Proxy  ::::::::\n")
-#     proxies = Proxies.query.filter_by(bad=0)
-#     for proxy in proxies:
-#         proxies_list.append(proxy.proxy)
-#     print(len(proxies_list))
-    
-#     if len(proxies_list) < 50 :
-#         URL = 'https://github.com/TheSpeedX/PROXY-List/blob/master/http.txt'
-#         page = requests.get(URL)
-#         soup = BeautifulSoup(page.content, 'html.parser')
-#         trs = soup.find_all(class_="blob-code blob-code-inner js-file-line")
-#         for tr in trs:
-#             if tr.get_text()[0] >= '0' and tr.get_text()[0] <= '9':
-#                 proxies_list.append(tr.get_text())
-#                 proxy = Proxies(tr.get_text(), 0)            
-#                 db.session.add(proxy)
-#                 db.session.commit()
-#     print(len(proxies_list))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5300, debug=True)
